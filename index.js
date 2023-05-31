e5db4e56de23ab9046660d666e912d3f33dfa829
d2412916 <nguyenvandung86420@gmail.com>
2023-05-31 23:22:40 +0700

    ->35

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 'use strict';
-
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const expressHandlebars = require('express-handlebars');
 const { createStarList } = require('./controllers/handlebarsHelper');
 const { createPagination } = require('express-handlebars-paginate');
+const session = require('express-session');
+const redisStore = require('connect-redis').default;
+const { createClient } = require('redis');
+const redisClient = createClient({
+    url: process.env.REDIS_URL
+})
+
+
+redisClient.connect().catch(console.error);
 
 // cau hinh public folder
 app.use(express.static(__dirname + '/public'));
@@ -26,11 +35,38 @@ app.engine('hbs', expressHandlebars.engine({
 }));
 app.set('view engine', 'hbs');
 
+// cau hinh doc du lieu post tu body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// cau hinh su dung session
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        maxAge: 20 * 60 * 1000 // 20ph
+    }
+}))
+
+// middlerware khoi tao gio hang
+app.use((req, res, next) => {
+    let Cart = require('./controllers/cart');
+    req.session.cart = new Cart(req.session.cart ? req.session.cart : {});
+    res.locals.quantity = req.session.cart.quantity;
+
+    next();
+})
+
 //  routes
 app.use('/', require('./routes/indexRouter.js'));
 app.use('/products', require('./routes/productsRouter'));
+app.use('/users', require('./routes/usersRouter'));
+
+
 app.use((req, res, next) => {
-    res.status(400).render('error', { message: 'File not found!' });
+    res.status(404).render('error', { message: 'File not found!' });
 })
 
 app.use((error, req, res, next) => {
